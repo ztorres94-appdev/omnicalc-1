c6af334d9ce9cdf6227e02a8e793b92088bf16f7
ztorres94 <ztorres@chicagobooth.edu>
2021-07-12 02:53:07 +0000

    first square forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
 
+def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+end
+
+def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+end
+
+def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb"})
+end
+
+def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 
+get( "/square/new", { :controller => "application", :action => "blank_square_form"})
+
+get( "/square_root/new", { :controller => "application", :action => "blank_square_root_form"})
+
+get( "/payment/new", { :controller => "application", :action => "blank_payment_form"})
+
+get( "/random/new", { :controller => "application", :action => "blank_random_form"})
+
 end
